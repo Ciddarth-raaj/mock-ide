524f51d140ba54cf5e51551007ef9e34c4859b0e
ciddarthjrj <121765398+ciddarthjrj@users.noreply.github.com>
2024-12-01 17:22:41 +0530

    feat: adds selecting tabs

--- src/components/Tabs/TabItem.js
+++ src/components/Tabs/TabItem.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 import styles from './styles.module.css';
+import { useDispatch } from 'react-redux';
+import { selectFile } from '../../redux/CodeEditor/editorActions';
 
 function TabItem({ filePath, isOpen }) {
+  const dispatch = useDispatch();
+
+  const handleTabClick = () => {
+    dispatch(selectFile('', filePath));
+  };
+
   return (
-    <div className={`${styles.tabItemContainer} ${isOpen ? styles.selected : ''}`}>
+    <div
+      className={`${styles.tabItemContainer} ${isOpen ? styles.selected : ''}`}
+      onClick={handleTabClick}
+    >
       <p>{filePath}</p>
 
       <p className={styles.closeButton}>×</p>
